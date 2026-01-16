dd095404b7c6c28e615437da5f13f80fd6eb3071
LastPerson07 <sharmadhanpal950@gmail.com>
2026-01-16 09:56:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,10 +9,11 @@ class Config:
     API_ID = int(os.getenv("API_ID"))
     API_HASH = os.getenv("API_HASH")
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+    MONGO_URI = os.getenv("MONGO_URI", "")
     BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID"))
 
-    INTRO_PHOTO = os.getenv("INTRO_PHOTO", "https://example.com/shield.jpg")
+    INTRO_PHOTO = os.getenv("INTRO_PHOTO", "https://i.postimg.cc/26ZBtBZr/13.png ")
     WELCOME_STICKER = os.getenv("WELCOME_STICKER")
+
 
     LOG_LEVEL = logging.INFO  
